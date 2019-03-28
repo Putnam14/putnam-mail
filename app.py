379032f9fbab816dd15ee680c61a79de7f0e525a
Putnam14 <putnambr76@gmail.com>
2019-03-27 22:24:13 -0600

    Custom 404 handler

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -34,6 +34,10 @@ def get_mail_by_id(mail_id):
     if len(single_mail) == 0:
         abort(404)
     return jsonify({'task': single_mail[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
