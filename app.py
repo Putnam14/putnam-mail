34cc81aff7908e052f56c8a45ad3380593c21678
Putnam14 <putnambr76@gmail.com>
2019-03-30 15:57:30 -0600

    Add DELETE and PUT endpoints, set id to 1 if no email exist

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ mail = [
         'name': u'Dan Abramov',
         'email': u'dan@abramov.com',
         'subject': u'Your site needs some state management',
-        'description': u'Have you heard of the library I wrote called Redux?'
+        'body': u'Have you heard of the library I wrote called Redux?'
     },
     {
         'id': 2,
         'name': u'Scout',
         'email': u'hey+scout@bridgerputnam.me',
         'subject': u'Need an allowance increase',
-        'description': u'When I bring the paper in, I deserve at least two treats. Maybe even a milkbone.'
+        'body': u'When I bring the paper in, I deserve at least two treats. Maybe even a milkbone.'
     }
 ]
 
@@ -24,7 +24,7 @@ def make_public_email(email):
     new_email = {}
     for field in email:
         if field == 'id':
-            new_email['uri'] = url_for('get_mail_by_id', mail_id=email['id'], _external=True)
+            new_email['uri'] = url_for('get_mail_by_id', mail_id=email['id'], _external=True) #_external displays the server root URL!
         else:
             new_email[field] = email[field]
     return new_email
@@ -45,13 +45,46 @@ def get_mail_by_id(mail_id):
         abort(404)
     return jsonify({'task': email[0]})
 
+@app.route('/mailer/api/v1.0/mail/<int:mail_id>', methods=['PUT'])
+def update_mail(mail_id):
+    email = [email for email in mail if email['id'] == mail_id]
+    if len(email) == 0 or not request.json:
+        abort(400)
+    if 'email' in request.json and type(request.json['email']) is not str:
+        abort(400)
+    if 'name' in request.json and type(request.json['name']) is not str:
+        abort(400)
+    if 'subject' in request.json and type(request.json['subject']) is not str:
+        abort(400)
+    if 'body' in request.json and type(request.json['body']) is not str:
+        abort(400)
+    email[0]['email'] = request.json.get('email',email[0]['email'])
+    email[0]['name'] = request.json.get('name',email[0]['name'])
+    email[0]['subject'] = request.json.get('subject',email[0]['subject'])
+    email[0]['body'] = request.json.get('body',email[0]['body'])
+    return jsonify({'email': email[0]})
+
+@app.route('/mailer/api/v1.0/mail/<int:mail_id>', methods=['DELETE'])
+def delete_mail(mail_id):
+    email = [email for email in mail if email['id'] == mail_id]
+    if len(email) == 0 or not request.json:
+        abort(400)
+    mail.remove(email[0])
+    return jsonify({'email_to_delete': email[0]})
+    #return jsonify({'result': True, 'message': 'Email deleted'})
+
+
 # This is the only one that does anything
 @app.route('/mailer/api/v1.0/mail', methods=['POST'])
 def send_mail():
     if not request.json or not 'subject' in request.json:
         abort(400)
+    if len(mail) == 0:
+        new_id = 1
+    else:
+        new_id = mail[-1]['id'] + 1
     email = {
-        'id': mail[-1]['id'] + 1,
+        'id': new_id,
         'email': request.json.get('email',""),
         'name': request.json.get('name',""),
         'subject': request.json['subject'],
@@ -59,7 +92,7 @@ def send_mail():
     }
     mail.append(email)
     #send_mail(email)
-    return jsonify({'sent': True}), 201
+    return jsonify({'result': True, 'message': 'Message sent'}), 201
 
 @app.errorhandler(404)
 def not_found(error):
