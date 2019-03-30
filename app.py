96e8fafe3a4fccc6e4e4215e47fe75b76b6010ac
Putnam14 <putnambr76@gmail.com>
2019-03-30 15:17:49 -0600

    POST with an ID, turn the ID into a public URI for GET

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
@@ -19,6 +19,16 @@ mail = [
         'description': u'When I bring the paper in, I deserve at least two treats. Maybe even a milkbone.'
     }
 ]
+
+def make_public_email(email):
+    new_email = {}
+    for field in email:
+        if field == 'id':
+            new_email['uri'] = url_for('get_mail_by_id', mail_id=email['id'], _external=True)
+        else:
+            new_email[field] = email[field]
+    return new_email
+
 # The following is example code, me playing around with making an API
 @app.route('/')
 def index():
@@ -26,14 +36,14 @@ def index():
 
 @app.route('/mailer/api/v1.0/mail', methods=['GET'])
 def get_mail():
-    return jsonify({'mail': mail})
+    return jsonify({'mail': [make_public_email(email) for email in mail]})
 
 @app.route('/mailer/api/v1.0/mail/<int:mail_id>', methods=['GET'])
 def get_mail_by_id(mail_id):
-    single_mail = [single_mail for single_mail in mail if single_mail['id'] == mail_id]
-    if len(single_mail) == 0:
+    email = [email for email in mail if email['id'] == mail_id]
+    if len(email) == 0:
         abort(404)
-    return jsonify({'task': single_mail[0]})
+    return jsonify({'task': email[0]})
 
 # This is the only one that does anything
 @app.route('/mailer/api/v1.0/mail', methods=['POST'])
@@ -41,6 +51,7 @@ def send_mail():
     if not request.json or not 'subject' in request.json:
         abort(400)
     email = {
+        'id': mail[-1]['id'] + 1,
         'email': request.json.get('email',""),
         'name': request.json.get('name',""),
         'subject': request.json['subject'],
