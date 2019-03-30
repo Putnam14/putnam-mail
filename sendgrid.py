08082e0aece61e6ff2a33051e7281ce9a03e29be
Putnam14 <putnambr76@gmail.com>
2019-03-30 17:28:14 -0600

    Build up Sendgrid's request body

--- sendgrid.py
+++ sendgrid.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 import os
+import requests
 
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', default=None)
 FROM_EMAIL = os.environ.get('FROM_EMAIL', default=None)
@@ -10,3 +11,44 @@ if not SENDGRID_API_KEY:
 
 if not FROM_EMAIL or not TO_EMAIL:
     raise ValueError("Need to set email info (FROM_EMAIL and TO_EMAIL")
+
+sendgrid_url = 'https://api.sendgrid.com/v3/mail/send'
+
+def build_request_body(email):
+    from_email = email['email']
+    name = email['name']
+    subject = email['subject']
+    body = email['body']
+    if from_email == "":
+        from_email = FROM_EMAIL
+    if name == "":
+        name = "Anonymous"
+    if subject == "":
+        subject = "Portfolio contact form message"
+    req_body = {
+        "personalizations": [
+            {
+                "to": [
+                    {
+                        "email": TO_EMAIL
+                    }
+                ],
+                "subject": subject
+            }
+        ],
+        "from": {
+            "email": from_email,
+            "name": name
+        },
+        "content": [
+            {
+                "type": "text/plain",
+                "value": body
+            }
+        ]
+    }
+    return req_body
+
+def send_mail(email):
+    email_body = build_request_body(email)
+    return email_body
